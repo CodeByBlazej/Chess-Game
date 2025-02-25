0dcf001678823c95c1e78c4615e71b60355e9959
CodeByBlazej <codebyblazej@gmail.com>
2025-02-25 19:20:45 +0000

    add initialize and play game method

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,15 @@
 require_relative 'game/players'
 require_relative 'game/board'
+
+class Game
+  attr_reader :board
+
+  def initialize
+    @board = Board.new(Array.new(8) { Array.new(8) })
+
+  end
+
+  def play_game
+    board.display_board
+  end
+end
