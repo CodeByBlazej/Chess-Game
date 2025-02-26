9c4d4e4651d2e2f1a9919c3d812635f4d79687f2
CodeByBlazej <codebyblazej@gmail.com>
2025-02-26 19:10:43 +0000

    create black and white board

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -1,25 +1,32 @@
 require_relative '../game'
 
 class Board
-  attr_reader :board
+  attr_reader :board, :white, :black
 
-  def initialize board
-    @board = board
+  def initialize 
+    @board = Array.new(8) { Array.new(8, '  ') }
+    @white = "\e[47m"
+    @black = "\e[40m"
+
   end
 
   def color_board
-    board.each_with_index do |row, idx|
-      row.map! { |el| idx.even? ? el = "\e[47m" : el = "\e[40m" }
+    board.each_with_index do |row, row_idx|
+      row.map!.with_index { |el, col_idx| (row_idx + col_idx).even? ? "\e[47m" : "\e[40m" }
     end
   end
 
   def display_board
-    color_board
-    board.each_with_index do |row, idx|
-      print 8 - idx
-      print row.join
-      puts "\n  ----------------------"
+    board.each_with_index do |row, row_idx|
+      print "#{8 - row_idx}"
+      
+      row.each_with_index do |cell, col_idx|
+        background = (row_idx + col_idx).even? ? white : black
+        print "#{background}#{cell}\e[0m"
+      end
+
+      puts
     end
-    puts "  a  b  c  d  e  f  g  h"
+    puts " A B C D E F G H "
   end
 end
