1e3a3d27b07474ea18ecc7b4852a51479bd10d81
CodeByBlazej <codebyblazej@gmail.com>
2025-02-26 19:36:59 +0000

    start adding tests for game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,17 @@
 require_relative '../lib/game'
+
+describe Game do
+  subject(:game) { described_class.new }
+
+  describe '#create_players' do
+    
+    before do
+      allow(game).to receive(:gets)  
+    end
+
+    it 'asks for names and creates 2 objects' do
+      expect(Players).to receive(:new).twice
+      game.create_players 
+    end
+  end
+end
