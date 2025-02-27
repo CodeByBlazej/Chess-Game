e01ca9bbd5b3a32747109c027e5db1acae051ad5
CodeByBlazej <codebyblazej@gmail.com>
2025-02-27 19:21:42 +0000

    finish create players method and add new play_round to test

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ require_relative 'game/players'
 require_relative 'game/board'
 
 class Game
-  attr_reader :board
+  attr_reader :board, :player1_name, :player2_name, :player1, :player2
 
   def initialize
     @board = Board.new
@@ -12,7 +12,27 @@ class Game
   def play_game
     introduction
     create_players
+    play_round
     board.display_board
+  end
+
+  def create_players
+    puts 'Player 1, what is your name?'
+    @player1_name = gets.chomp
+    puts 'Player 2, what is your name?'
+    @player2_name = gets.chomp
+
+    @player1 = Players.new(player1_name)
+    @player2 = Players.new(player2_name)
+  end
+
+  def play_round
+    board.board[0][1] = "\u2655 "
+    board.board[0][2] = "\u265A "
+    board.board[0][3] = "\u2654 "
+    board.board[0][5] = "\u265A "
+
+
   end
 
   def introduction
