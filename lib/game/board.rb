ea26078c3376c32892ac079e661d1a19181c6c15
CodeByBlazej <codebyblazej@gmail.com>
2025-02-28 18:53:19 +0000

    add name cells method

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -1,15 +1,16 @@
 require_relative '../game'
 
 class Board
-  attr_reader :board, :white, :black, :knight
+  attr_reader :board, :white, :black, :knight, :cell_names
 
   def initialize 
     @board = Array.new(8) { Array.new(8, '  ') }
     @white = "\e[0;100m"
     @black = "\e[40m"
     @knight = Knight.new 
+    @cell_names = {}
   end
-
+  
   def display_board
     board.rotate.each_with_index do |row, row_idx|
       print "#{8 - row_idx}"
@@ -23,4 +24,15 @@ class Board
     end
     puts " A B C D E F G H "
   end
+
+  def name_cells
+    alphabet = ('A'..'H').to_a
+
+    board.board.each_with_index do |row, row_idx|
+      row.each_with_index do |cell, col_idx|
+        cell_names[]
+      end
+    end
+  end
+
 end
