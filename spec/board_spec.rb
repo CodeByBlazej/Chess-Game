83520944dfcf3f02ff496233e0308e6638f104bb
CodeByBlazej <codebyblazej@gmail.com>
2025-03-01 14:21:25 +0000

    turn coordinated round again to match board nomenclature not A1

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,10 +7,10 @@ describe Board do
     it 'gives each cell a name and saves it in hash' do
       board.name_cells
       expect(board.cell_names['A1']).to eq([0, 0])
-      expect(board.cell_names['B1']).to eq([1, 0])
-      expect(board.cell_names['F8']).to eq([5, 7])
-      expect(board.cell_names['H4']).to eq([7, 3])
-      expect(board.cell_names['D5']).to eq([3, 4])
+      expect(board.cell_names['B1']).to eq([0, 1])
+      expect(board.cell_names['F8']).to eq([7, 5])
+      expect(board.cell_names['H4']).to eq([3, 7])
+      expect(board.cell_names['D5']).to eq([4, 3])
     end
   end
 end
