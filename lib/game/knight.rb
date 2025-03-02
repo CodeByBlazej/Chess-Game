582422a8a62a76bbc27da8273ff6392e3962f693
CodeByBlazej <codebyblazej@gmail.com>
2025-03-02 13:34:54 +0000

    add starting position to attr reader

--- lib/game/knight.rb
+++ lib/game/knight.rb
@@ -1,12 +1,12 @@
 require_relative '../game'
 
 class Knight
-  attr_reader :color, :symbol
+  attr_reader :color, :symbol, :starting_position
 
-  def initialize color, symbol
+  def initialize color, symbol, starting_position
     @color = color
     @symbol = symbol
-    @starting_position = nil
+    @starting_position = starting_position
     @current_position = nil
   end
 end
