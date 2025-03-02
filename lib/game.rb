9c711236f672ac00241d48720a92b3e6bce31801
CodeByBlazej <codebyblazej@gmail.com>
2025-03-02 13:34:10 +0000

    add code for creating rooks

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,11 @@
 require_relative 'game/players'
 require_relative 'game/board'
 require_relative 'game/knight'
+require_relative 'game/rook'
 require 'pry-byebug'
 
 class Game
-  attr_reader :board, :player1_name, :player2_name, :player1, :player2
+  attr_reader :board, :player1_name, :player2_name, :player1, :player2, :white_chesspieces_positions, :black_chesspieces_positions
 
   def initialize
     @board = Board.new
@@ -15,13 +16,15 @@ class Game
   def play_game
     introduction
     create_players
-    play_round
+    # play_round
     board.display_board
     puts board.board[0][1]
     puts board.board[1][0]
     # board.name_cells
     create_chesspieces_and_add_to_board('white')
     create_chesspieces_and_add_to_board('black')
+    board.display_board
+
     # create chesspieces - colors and their coordinates
   end
 
@@ -37,9 +40,22 @@ class Game
 
   def create_chesspieces_and_add_to_board(color)
     create_chesspieces_positions(color)
-    @knight = Knight.new(color, "\u2658 ")
 
-    board.board[0][1] = @knight.symbol
+    # @knight = Knight.new(color, "\u2658 ")
+
+    # board.board[0][1] = @knight.symbol
+    color == 'white' ? col_pos = white_chesspieces_positions : col_pos = black_chesspieces_positions
+
+    col_pos.each_pair do |key, values|
+      case key
+      when :rook
+        values.each do |value|
+          # binding.pry
+          rook = Rook.new(color, value)
+          board.board[board.cell_names[value][0]][board.cell_names[value][1]] = rook.symbol
+        end
+      end
+    end 
   end
 
   def create_chesspieces_positions(color)
