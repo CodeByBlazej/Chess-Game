2432fa816643bb1b607d387bc1c454b46e458b41
CodeByBlazej <codebyblazej@gmail.com>
2025-03-02 09:17:06 +0000

    swap strings to symbols to make it easier

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,11 +6,11 @@ describe Board do
   describe '#name_cells' do
     it 'gives each cell a name and saves it in hash' do
       board.name_cells
-      expect(board.cell_names['A1']).to eq([0, 0])
-      expect(board.cell_names['B1']).to eq([0, 1])
-      expect(board.cell_names['F8']).to eq([7, 5])
-      expect(board.cell_names['H4']).to eq([3, 7])
-      expect(board.cell_names['D5']).to eq([4, 3])
+      expect(board.cell_names[:A1]).to eq([0, 0])
+      expect(board.cell_names[:B1]).to eq([0, 1])
+      expect(board.cell_names[:F8]).to eq([7, 5])
+      expect(board.cell_names[:H4]).to eq([3, 7])
+      expect(board.cell_names[:D5]).to eq([4, 3])
     end
   end
 end
