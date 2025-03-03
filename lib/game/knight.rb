3414a763c9424de94ce03b99232d5aa42a14cb02
CodeByBlazej <codebyblazej@gmail.com>
2025-03-03 17:46:52 +0000

    change the way symbol is initialized

--- lib/game/knight.rb
+++ lib/game/knight.rb
@@ -3,9 +3,9 @@ require_relative '../game'
 class Knight
   attr_reader :color, :symbol, :starting_position
 
-  def initialize color, symbol, starting_position
+  def initialize color, starting_position
     @color = color
-    @symbol = symbol
+    @symbol = color == 'white' ? "\u2658 " : "\u265E "
     @starting_position = starting_position
     @current_position = nil
   end
