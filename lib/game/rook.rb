e891b23313a34bc46857c50f16f839842d3a8560
CodeByBlazej <codebyblazej@gmail.com>
2025-03-09 08:55:10 +0000

    update initialize method

--- lib/game/rook.rb
+++ lib/game/rook.rb
@@ -1,12 +1,19 @@
 require_relative '../game'
+require 'pry-byebug'
 
 class Rook
-  attr_reader :color, :symbol, :starting_position
+  attr_reader :color, :symbol, :starting_position_cell, :current_position, :board, :all_moves
 
-  def initialize color, starting_position
+  def initialize color, starting_position_cell, board
+    @board = board
     @color = color
     @symbol = color == 'white' ? "\u2656 " : "\u265C "
-    @starting_position = starting_position
-    @current_position = nil
+    @starting_position_cell = starting_position_cell
+    @current_position = board.cell_names[starting_position_cell]
+    @all_moves = nil
+  end
+
+  def available_moves
+    
   end
 end
