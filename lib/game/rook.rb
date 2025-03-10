f6498bc2ba45c5f2b90f5dda8dae53709bbff53b
CodeByBlazej <codebyblazej@gmail.com>
2025-03-10 19:53:03 +0000

    finish available_moves and add 2 new methods

--- lib/game/rook.rb
+++ lib/game/rook.rb
@@ -25,28 +25,52 @@ class Rook
     reachable = []
 
     directions.each do |dr, dc|
-      
+      r, c = row, col
+
       loop do
-        row += dr
-        col += dc
+        r += dr
+        c += dc
 
-        break unless row.between?(0, 7) && col.between?(0, 7)
+        break unless r.between?(0, 7) && c.between?(0, 7)
 
-        cell_name = board.cell_names.key([row, col])
+        cell_name = board.cell_names.key([r, c])
         occupant = board.chesspiece[cell_name]
 
         if occupant.nil?
-          reachable << [row, col]
-        else
+          reachable << [r, c]
+        else 
           if occupant.color != color
-            reachable << [row, col]
+            reachable << [r, c]
           end
           break
         end
       end
     end
 
-    reachable
+    @all_moves = reachable
+  end
+
+  def moves(to)
+    available_moves
+    cell_name = board.cell_names.key(to)
+    occupant = board.chesspiece[cell_name]
+
+    if all_moves.any?(to)
+      chesspiece_moves(to, cell_name)
+      return
+    elsif occupant && occupant.color == color && same_row_and_col
+      puts "This cell is occupied by other chesspiece of yours! Please select other cell..."
+    else
+      puts 'You cannot make this move!'
+    end
+  end
+
+  def chesspiece_moves(to, cell_name)
+    @board.board[current_position[0]][current_position[1]] = '  '
+    @board.chesspiece[starting_position_cell] = nil
+    @current_position = to
+    @board.board[to[0]][to[1]] = symbol
+    @board.chesspiece[cell_name] = self
   end
 
 end
