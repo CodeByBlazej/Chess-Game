3c86b16709932ccff4fbe33cececa3af0be19b0e
CodeByBlazej <codebyblazej@gmail.com>
2025-03-11 19:27:10 +0000

    add 2 new methods

--- lib/game/pawn.rb
+++ lib/game/pawn.rb
@@ -1,12 +1,68 @@
 require_relative '../game'
+require 'pry-byebug'
 
 class Pawn
-  attr_reader :color, :symbol, :starting_position
+  attr_reader :color, :symbol, :starting_position_cell, :current_position, :board, :all_moves
 
-  def initialize color, starting_position
+  def initialize color, starting_position_cell, board
+    @board = board
     @color = color
     @symbol = color == 'white' ? "\u2659 " : "\u265F "
-    @starting_position = starting_position
-    @current_position = nil
+    @starting_position_cell = starting_position_cell
+    @current_position = board.cell_names[starting_position_cell]
+    @all_moves = nil
+  end
+
+  def available_moves
+    directions = color == 'white' ? [[-1, 0], [-1, -1], [-1, 1]] : [[1, 0], [1, 1], [1, -1]]
+
+    row, col = current_position
+    reachable = []
+    iteration = 0
+
+    directions.each do |dr, dc|
+      r, c = row, col
+      iteration += 1
+
+      loop do
+        r += dr
+        c += dc
+
+        break unless r.between?(0, 7) && c.between?(0, 7)
+
+        cell_name = board.cell_names.key([r, c])
+        occupant = board.chesspiece[cell_name]
+
+        if occupant.nil? && iteration == 1
+          reachable << [r, c]
+          break
+        elsif occupant && occupant.color != color && iteration > 1
+          reachable << [r, c]
+          break
+        end
+      end
+
+      @all_moves = reachable
+    end
+  end
+
+  def moves(to)
+    available_moves
+    cell_name = board.cell_names.key(to)
+
+    if all_moves.any?(to)
+      chesspiece_moves(to, cell_name)
+      return
+    else
+      puts 'You cannot make this move!'
+    end
+  end
+
+  def chesspiece_moves(to, cell_name)
+    @board.board[current_position[0]][current_position[1]] = '  '
+    @board.chesspiece[starting_position_cell] = nil
+    @current_position = to
+    @board.board[to[0]][to[1]] = symbol
+    @board.chesspiece[cell_name] = self
   end
 end
