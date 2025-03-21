891ed951cf98a42dc4f1a5726987e1bd2f2ccc4b
CodeByBlazej <codebyblazej@gmail.com>
2025-03-21 06:59:31 +0000

    add way_to_king variable

--- lib/game/bishop.rb
+++ lib/game/bishop.rb
@@ -2,7 +2,7 @@ require_relative '../game'
 require 'pry-byebug'
 
 class Bishop
-  attr_reader :color, :symbol, :starting_position_cell, :current_position, :board, :all_moves
+  attr_reader :color, :symbol, :starting_position_cell, :current_position, :board, :all_moves, :way_to_king
 
   def initialize color, starting_position_cell, board
     @board = board
@@ -11,6 +11,7 @@ class Bishop
     @starting_position_cell = starting_position_cell
     @current_position = board.cell_names[starting_position_cell]
     @all_moves = nil
+    @way_to_king = nil
   end
 
   def available_moves
@@ -23,6 +24,7 @@ class Bishop
 
     row, col = current_position
     reachable = []
+    way = []
 
     directions.each do |dr, dc|
       r, c = row, col
@@ -39,12 +41,17 @@ class Bishop
         if occupant.nil?
           reachable << [r, c]
         else
-          if occupant.color != color
+          if occupant.color != color && (occupant.symbol == "\u2654 " || occupant.symbol == "\u265A ")
+            reachable << [r, c]
+            way << [r, c]
+            @way_to_king = way.dup
+          elsif occupant.color != color && occupant.symbol != "\u2654 " && occupant.symbol != "\u265A "
             reachable << [r, c]
           end
           break
         end
       end
+      way.clear
     end
 
     @all_moves = reachable
