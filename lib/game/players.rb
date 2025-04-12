f7c35ef773f786a62d3e95a10b817213fc7585c6
CodeByBlazej <codebyblazej@gmail.com>
2025-04-12 09:07:43 +0100

    add as json and from json methods

--- lib/game/players.rb
+++ lib/game/players.rb
@@ -7,4 +7,15 @@ class Players
     @name = name
     @color = color
   end
+
+  def as_json
+    {
+      name: @name,
+      color: @color
+    }
+  end
+
+  def self.from_json(data)
+    new(data['name'], data['color'])
+  end
 end 
