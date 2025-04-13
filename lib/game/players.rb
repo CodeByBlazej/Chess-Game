02e4f5da9279fcb6180afc7f0b3bc6bcde350466
CodeByBlazej <codebyblazej@gmail.com>
2025-04-13 09:25:59 +0100

    adjust json methods to match with the others

--- lib/game/players.rb
+++ lib/game/players.rb
@@ -15,7 +15,14 @@ class Players
     }
   end
 
+  def to_json(*_args)
+    as_json.to_json
+  end
+
   def self.from_json(data)
-    new(data['name'], data['color'])
+    instance = new
+    instance.instance_variable_set(:@name, data['name'])
+    instance.instance_variable_set(:@color, data['color'])
+    instance
   end
 end 
