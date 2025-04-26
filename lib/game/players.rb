e1d42bf7fb4e9aba595ada11e35268e1dcd24314
CodeByBlazej <codebyblazej@gmail.com>
2025-04-26 12:46:25 +0100

    shorten from json method

--- lib/game/players.rb
+++ lib/game/players.rb
@@ -20,9 +20,6 @@ class Players
   end
 
   def self.from_json(data)
-    instance = new
-    instance.instance_variable_set(:@name, data['name'])
-    instance.instance_variable_set(:@color, data['color'])
-    instance
+    new(data['name'], data['color'])
   end
 end 
